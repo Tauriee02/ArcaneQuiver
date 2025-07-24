fa7d292b73ffb957d940667bf40f7c32c8cd7927
Tauriee02 <silvia.silviacolilli@outlook.it>
2025-07-24 17:04:26 +0200

    fix projectile

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -7,9 +7,22 @@ public class Projectile : MonoBehaviour
     public float speed = 5f;
     public Vector2 direction;
 
+     private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = direction.normalized * speed;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
     void Update()
     {
-        transform.Translate(direction.normalized * speed * Time.deltaTime);
+        transform.position += (Vector3)(direction.normalized * speed * Time.deltaTime);
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 
     void OnBecameInvisible()
